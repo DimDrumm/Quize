35fdda8b52616d6de70041c6039d4bbd2020957b
ITSTEP\st <stepjava2@gmail.com>
2019-04-29 20:20:09 +0300

    +menu

--- app/src/main/java/a2018/by/step/myquiz/activity/IntroActivity.kt
+++ app/src/main/java/a2018/by/step/myquiz/activity/IntroActivity.kt
@@ -1,13 +1,35 @@
 package a2018.by.step.myquiz.activity
 
 import a2018.by.step.myquiz.R
+import a2018.by.step.myquiz.data.SharedPreferencesHelper
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class IntroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menu_logout -> logout()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun logout() {
+        SharedPreferencesHelper.clearData(applicationContext)
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
 }
