95d5365614d67b2bc396bb5ffefeeb9daa6241b7
ITSTEP\st <stepjava2@gmail.com>
2019-05-15 20:31:18 +0300

    For beginners

--- app/src/main/java/a2018/by/step/myquiz/activity/QuestionActivity.kt
+++ app/src/main/java/a2018/by/step/myquiz/activity/QuestionActivity.kt
@@ -1,16 +1,19 @@
 package a2018.by.step.myquiz.activity
 
 import a2018.by.step.myquiz.R
-import android.support.v7.app.AppCompatActivity
+import a2018.by.step.myquiz.fragment.SingleSelectionFragment
 import android.os.Bundle
-import android.view.Menu
 
 class QuestionActivity : BaseMenuActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
-
+        supportFragmentManager.beginTransaction()
+            .replace(
+                R.id.container_fragment,
+                SingleSelectionFragment()
+            )
+            .commit()
     }
-
 }
