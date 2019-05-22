cd950961224e5b27228f1f2cb955f782b298be08
Nastya2002 <an.usoltseva2002@gmail.com>
2019-05-22 21:16:24 +0300

    lost files)

--- app/src/main/java/a2018/by/step/myquiz/activity/QuestionActivity.kt
+++ app/src/main/java/a2018/by/step/myquiz/activity/QuestionActivity.kt
@@ -3,21 +3,50 @@ package a2018.by.step.myquiz.activity
 import a2018.by.step.myquiz.R
 import a2018.by.step.myquiz.data.QuestionRepository
 import a2018.by.step.myquiz.fragment.SingleSelectionFragment
+import a2018.by.step.myquiz.fragment.TextQuestionFragment
 import a2018.by.step.myquiz.model.Question
+import android.content.Context
 import android.os.Bundle
+import timber.log.Timber
 
 class QuestionActivity : BaseMenuActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Timber.d("onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
+       if (savedInstanceState == null) {
+           val q = QuestionRepository.getQuestions()[0]
+           supportFragmentManager.beginTransaction()
+               .replace(
+                   R.id.container_fragment,
+                   TextQuestionFragment.newInstance(/*q*/)
+               )
+       }
+    }
 
-      val q = QuestionRepository.getQuestions()[0]
-        supportFragmentManager.beginTransaction()
-            .replace(
-                R.id.container_fragment,
-                SingleSelectionFragment()
-            )
-            .commit()
+    override fun onStart() {
+        super.onStart()
+        Timber.d("onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.d("onStop")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.d("onResume ${hashCode()}")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.d("onPause ${hashCode()}")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.d("onDestroy")
     }
 }
--- app/src/main/java/a2018/by/step/myquiz/fragment/TextQuestionFragment.kt
+++ app/src/main/java/a2018/by/step/myquiz/fragment/TextQuestionFragment.kt
@@ -8,7 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 
 import a2018.by.step.myquiz.R
-import a2018.by.step.myquiz.model.Question
+import a2018.by.step.myquiz.data.QuestionRepository
+import android.content.Context
+import kotlinx.android.synthetic.main.fragment_text_question.view.*
+import timber.log.Timber
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,10 +24,11 @@ private const val ARG_QUESTION = "question"
  *
  */
 class TextQuestionFragment : Fragment() {
-    private var question: Question<*>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Timber.d("onCreate")
+        Timber.d("${Companion.question}")
 //        arguments?.let {
 //            question = it.getString(ARG_QUESTION)
 //        }
@@ -34,27 +38,57 @@ class TextQuestionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Timber.d("onCreateView")
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text_question, container, false)
+        val view = inflater.inflate(R.layout.fragment_text_question,container,false)
+        view.tv_question_text.text.
+        return view
     }
 
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        Timber.d("onAttach")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Timber.d("onDetach")
+    }
+    override fun onStart() {
+        super.onStart()
+        Timber.d("onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.d("onStop")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.d("onResume ${hashCode()}")
+        Timber.d("Arguments ${arguments?.getString(ARG_QUESTION)}")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.d("onPause ${hashCode()}")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.d("onDestroy")
+    }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TextQuestionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+        val question = QuestionRepository.getQuestions()[1]
         @JvmStatic
-        fun newInstance(question: Question<*>) =
+        fun newInstance(/*question: Question<*>*/) =
             TextQuestionFragment().apply {
+                Timber.d("newInstance")
                 arguments = Bundle().apply {
-                    putString(ARG_QUESTION,question)
                 }
             }
     }
+
 }
